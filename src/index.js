6f005b9dc52f4a95778b25b6e6cb0d733aeaf628
github cjl248 <github cjl248@cornell.edu>
2019-08-29 13:58:09 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const config = {}
+
+document.addEventListener('DOMContentLoaded', function(){
+
+  const request = fetch(imgUrl, config)
+  .then(response => response.json()) //JSON.stringify?
+    .then(addDogPics)
+
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+  fetch(breedUrl, config)
+    .then(response => response.json())
+      .then(addDogBreeds)
+
+  document.querySelector("#dog-breeds").addEventListener("click", changeColor)
+})
+
+function addDogPics(data) {
+  dogDiv = document.querySelector("#dog-image-container")
+  data["message"].forEach(function(dogSRC) {
+    const img = document.createElement("img"); img.src = dogSRC
+    dogDiv.appendChild(img)
+  })
+}
+
+function addDogBreeds(data) {
+  const message = data["message"]
+  dogUL = document.querySelector("#dog-breeds")
+  console.log(message)
+  for (const outerBreed in message) {
+    const li = document.createElement("li")
+    li.innerHTML = outerBreed
+    dogUL.appendChild(li)
+  }
+}
+
+function changeColor(){
+  const target = event.target
+  if (target.nodeName === "LI") {
+    if (target.style.color === "green") {
+      target.style.color = "black"
+    } else {
+      target.style.color = "green"
+    }
+  }
+}
